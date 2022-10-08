618abf915635a266bdd5811d3b9ee0335e8c2cc3
jejbuitenhuis <jorambuitenhuis@gmail.com>
2022-10-08 13:38:39 +0200

    Reformat test code

--- src/test/java/nl/han/ica/icss/parser/Fixtures.java
+++ src/test/java/nl/han/ica/icss/parser/Fixtures.java
@@ -11,9 +11,10 @@ import nl.han.ica.icss.ast.selectors.ClassSelector;
 import nl.han.ica.icss.ast.selectors.IdSelector;
 import nl.han.ica.icss.ast.selectors.TagSelector;
 
-public class Fixtures {
-
-    public static AST uncheckedLevel0() {
+public class Fixtures
+{
+	public static AST uncheckedLevel0()
+	{
 		Stylesheet stylesheet = new Stylesheet();
 		/*
 		p {
@@ -22,11 +23,11 @@ public class Fixtures {
 		}
 		*/
 		stylesheet.addChild((new Stylerule())
-				.addChild(new TagSelector("p"))
-				.addChild((new Declaration("background-color"))
-                        .addChild(new ColorLiteral("#ffffff")))
-				.addChild((new Declaration("width"))
-						.addChild(new PixelLiteral("500px")))
+			.addChild(new TagSelector("p"))
+			.addChild((new Declaration("background-color"))
+				.addChild(new ColorLiteral("#ffffff")))
+			.addChild((new Declaration("width"))
+				.addChild(new PixelLiteral("500px")))
 		);
 		/*
 		a {
@@ -34,9 +35,9 @@ public class Fixtures {
 		}
 		*/
 		stylesheet.addChild((new Stylerule())
-				.addChild(new TagSelector("a"))
-				.addChild((new Declaration("color"))
-						.addChild(new ColorLiteral("#ff0000")))
+			.addChild(new TagSelector("a"))
+			.addChild((new Declaration("color"))
+				.addChild(new ColorLiteral("#ff0000")))
 		);
 		/*
 		#menu {
@@ -44,9 +45,9 @@ public class Fixtures {
 		}
 		*/
 		stylesheet.addChild((new Stylerule())
-				.addChild(new IdSelector("#menu"))
-				.addChild((new Declaration("width"))
-						.addChild(new PixelLiteral("520px")))
+			.addChild(new IdSelector("#menu"))
+			.addChild((new Declaration("width"))
+				.addChild(new PixelLiteral("520px")))
 		);
 		/*
 		.menu {
@@ -54,15 +55,16 @@ public class Fixtures {
 		}
 		*/
 		stylesheet.addChild((new Stylerule())
-				.addChild(new ClassSelector(".menu"))
-				.addChild((new Declaration("color"))
-						.addChild(new ColorLiteral("#000000")))
+			.addChild(new ClassSelector(".menu"))
+			.addChild((new Declaration("color"))
+				.addChild(new ColorLiteral("#000000")))
 		);
 
-        return new AST(stylesheet);
-    }
+		return new AST(stylesheet);
+	}
 
-	public static AST uncheckedLevel1() {
+	public static AST uncheckedLevel1()
+	{
 		Stylesheet stylesheet = new Stylesheet();
 		/*
 			LinkColor := #ff0000;
@@ -71,60 +73,60 @@ public class Fixtures {
 			UseLinkColor := FALSE;
 		 */
 		stylesheet.addChild((new VariableAssignment())
-                .addChild(new VariableReference("LinkColor"))
-                .addChild(new ColorLiteral("#ff0000"))
-        );
-		stylesheet.addChild((new VariableAssignment())
-                .addChild(new VariableReference("ParWidth"))
-                .addChild(new PixelLiteral("500px"))
-        );
-		stylesheet.addChild((new VariableAssignment())
-				.addChild(new VariableReference("AdjustColor"))
-				.addChild(new BoolLiteral(true))
+			.addChild(new VariableReference("LinkColor"))
+			.addChild(new ColorLiteral("#ff0000"))
 		);
 		stylesheet.addChild((new VariableAssignment())
-				.addChild(new VariableReference("UseLinkColor"))
-				.addChild(new BoolLiteral(false))
+			.addChild(new VariableReference("ParWidth"))
+			.addChild(new PixelLiteral("500px"))
 		);
-   	    /*
-	        p {
-	        background-color: #ffffff;
-	        width: ParWidth;
-            }
-	    */
-        stylesheet.addChild((new Stylerule())
-            .addChild(new TagSelector("p"))
-            .addChild((new Declaration("background-color"))
-                    .addChild(new ColorLiteral("#ffffff")))
-            .addChild((new Declaration("width"))
-                    .addChild(new VariableReference("ParWidth")))
-        );
-        /*
-        a {
-	        color: LinkColor;
-        }
-        */
-        stylesheet.addChild((new Stylerule())
+		stylesheet.addChild((new VariableAssignment())
+			.addChild(new VariableReference("AdjustColor"))
+			.addChild(new BoolLiteral(true))
+		);
+		stylesheet.addChild((new VariableAssignment())
+			.addChild(new VariableReference("UseLinkColor"))
+			.addChild(new BoolLiteral(false))
+		);
+		/*
+			p {
+			background-color: #ffffff;
+			width: ParWidth;
+			}
+		*/
+		stylesheet.addChild((new Stylerule())
+			.addChild(new TagSelector("p"))
+			.addChild((new Declaration("background-color"))
+				.addChild(new ColorLiteral("#ffffff")))
+			.addChild((new Declaration("width"))
+				.addChild(new VariableReference("ParWidth")))
+		);
+		/*
+		a {
+			color: LinkColor;
+		}
+		*/
+		stylesheet.addChild((new Stylerule())
 			.addChild(new TagSelector("a"))
 			.addChild((new Declaration("color"))
 				.addChild(new VariableReference("LinkColor")))
 		);
-        /*
-            #menu {
-	            width: 520px;
-            }
-        */
-        stylesheet.addChild((new Stylerule())
+		/*
+			#menu {
+				width: 520px;
+			}
+		*/
+		stylesheet.addChild((new Stylerule())
 			.addChild(new IdSelector("#menu"))
 			.addChild((new Declaration("width"))
 				.addChild(new PixelLiteral("520px")))
 		);
-        /*
-            .menu {
-	            color: #000000;
-            }
-        */
-        stylesheet.addChild((new Stylerule())
+		/*
+			.menu {
+				color: #000000;
+			}
+		*/
+		stylesheet.addChild((new Stylerule())
 			.addChild(new ClassSelector(".menu"))
 			.addChild((new Declaration("color"))
 				.addChild(new ColorLiteral("#000000")))
@@ -132,7 +134,8 @@ public class Fixtures {
 		return new AST(stylesheet);
 	}
 
-	public static AST uncheckedLevel2() {
+	public static AST uncheckedLevel2()
+	{
 		Stylesheet stylesheet = new Stylesheet();
 		/*
 			LinkColor := #ff0000;
@@ -141,65 +144,65 @@ public class Fixtures {
 			UseLinkColor := FALSE;
 		 */
 		stylesheet.addChild((new VariableAssignment())
-				.addChild(new VariableReference("LinkColor"))
-				.addChild(new ColorLiteral("#ff0000"))
+			.addChild(new VariableReference("LinkColor"))
+			.addChild(new ColorLiteral("#ff0000"))
 		);
 		stylesheet.addChild((new VariableAssignment())
-				.addChild(new VariableReference("ParWidth"))
-				.addChild(new PixelLiteral("500px"))
+			.addChild(new VariableReference("ParWidth"))
+			.addChild(new PixelLiteral("500px"))
 		);
 		stylesheet.addChild((new VariableAssignment())
-				.addChild(new VariableReference("AdjustColor"))
-				.addChild(new BoolLiteral(true))
+			.addChild(new VariableReference("AdjustColor"))
+			.addChild(new BoolLiteral(true))
 		);
 		stylesheet.addChild((new VariableAssignment())
-				.addChild(new VariableReference("UseLinkColor"))
-				.addChild(new BoolLiteral(false))
+			.addChild(new VariableReference("UseLinkColor"))
+			.addChild(new BoolLiteral(false))
 		);
-   	    /*
-	        p {
-	        background-color: #ffffff;
-	        width: ParWidth;
-            }
-	    */
-        stylesheet.addChild((new Stylerule())
-            .addChild(new TagSelector("p"))
-            .addChild((new Declaration("background-color"))
-                    .addChild(new ColorLiteral("#ffffff")))
-            .addChild((new Declaration("width"))
-                    .addChild(new VariableReference("ParWidth")))
-        );
-        /*
-        a {
-	        color: LinkColor;
-        }
-        */
-        stylesheet.addChild((new Stylerule())
+		/*
+			p {
+			background-color: #ffffff;
+			width: ParWidth;
+			}
+		*/
+		stylesheet.addChild((new Stylerule())
+			.addChild(new TagSelector("p"))
+			.addChild((new Declaration("background-color"))
+				.addChild(new ColorLiteral("#ffffff")))
+			.addChild((new Declaration("width"))
+				.addChild(new VariableReference("ParWidth")))
+		);
+		/*
+		a {
+			color: LinkColor;
+		}
+		*/
+		stylesheet.addChild((new Stylerule())
 			.addChild(new TagSelector("a"))
 			.addChild((new Declaration("color"))
 				.addChild(new VariableReference("LinkColor")))
 		);
-        /*
-            #menu {
-        	width: ParWidth + 2 * 10px;
-            }
-        */
-        stylesheet.addChild((new Stylerule())
+		/*
+			#menu {
+			width: ParWidth + 2 * 10px;
+			}
+		*/
+		stylesheet.addChild((new Stylerule())
 			.addChild(new IdSelector("#menu"))
 			.addChild((new Declaration("width"))
 				.addChild((new AddOperation())
-                        .addChild(new VariableReference("ParWidth"))
-                        .addChild((new MultiplyOperation())
-                                .addChild(new ScalarLiteral("2") )
-                                .addChild(new PixelLiteral("10px"))
+					.addChild(new VariableReference("ParWidth"))
+					.addChild((new MultiplyOperation())
+						.addChild(new ScalarLiteral("2") )
+						.addChild(new PixelLiteral("10px"))
 
-        ))));
-        /*
-            .menu {
-	            color: #000000;
-            }
-        */
-        stylesheet.addChild((new Stylerule())
+		))));
+		/*
+			.menu {
+				color: #000000;
+			}
+		*/
+		stylesheet.addChild((new Stylerule())
 			.addChild(new ClassSelector(".menu"))
 			.addChild((new Declaration("color"))
 				.addChild(new ColorLiteral("#000000")))
@@ -207,7 +210,8 @@ public class Fixtures {
 		return new AST(stylesheet);
 	}
 
-	public static AST uncheckedLevel3() {
+	public static AST uncheckedLevel3()
+	{
 		Stylesheet stylesheet = new Stylesheet();
 		/*
 			LinkColor := #ff0000;
@@ -216,112 +220,106 @@ public class Fixtures {
 			UseLinkColor := FALSE;
 		 */
 		stylesheet.addChild((new VariableAssignment())
-				.addChild(new VariableReference("LinkColor"))
-				.addChild(new ColorLiteral("#ff0000"))
+			.addChild(new VariableReference("LinkColor"))
+			.addChild(new ColorLiteral("#ff0000"))
 		);
 		stylesheet.addChild((new VariableAssignment())
-				.addChild(new VariableReference("ParWidth"))
-				.addChild(new PixelLiteral("500px"))
+			.addChild(new VariableReference("ParWidth"))
+			.addChild(new PixelLiteral("500px"))
 		);
 		stylesheet.addChild((new VariableAssignment())
-				.addChild(new VariableReference("AdjustColor"))
-				.addChild(new BoolLiteral(true))
+			.addChild(new VariableReference("AdjustColor"))
+			.addChild(new BoolLiteral(true))
 		);
 		stylesheet.addChild((new VariableAssignment())
-				.addChild(new VariableReference("UseLinkColor"))
-				.addChild(new BoolLiteral(false))
+			.addChild(new VariableReference("UseLinkColor"))
+			.addChild(new BoolLiteral(false))
 		);
-   	    /*
-	        p {
+		/*
+			p {
 				background-color: #ffffff;
 				width: ParWidth;
 				if[AdjustColor] {
-	    			color: #124532;
-	    			if[UseLinkColor]{
-	        			bg-color: LinkColor;
-	    			}
+					color: #124532;
+					if[UseLinkColor]{
+						bg-color: LinkColor;
+					}
 				}
 			}
 			p {
 				background-color: #ffffff;
 				width: ParWidth;
 				if[AdjustColor] {
-	    			color: #124532;
-	    		if[UseLinkColor]{
-	        		background-color: LinkColor;
-	    		} else {
-	        		background-color: #000000;
-	    		}
-	    		height: 20px;
+					color: #124532;
+				if[UseLinkColor]{
+					background-color: LinkColor;
+				} else {
+					background-color: #000000;
+				}
+				height: 20px;
 			}
 }
-	    */
-        stylesheet.addChild((new Stylerule())
-				.addChild(new TagSelector("p"))
-					.addChild((new Declaration("background-color"))
-							.addChild(new ColorLiteral("#ffffff")))
-					.addChild((new Declaration("width"))
-							.addChild(new VariableReference("ParWidth")))
-					.addChild((new IfClause())
-						.addChild(new VariableReference("AdjustColor"))
-						.addChild((new Declaration("color")
-								.addChild(new ColorLiteral("#124532"))))
-							.addChild((new IfClause())
-									.addChild(new VariableReference("UseLinkColor"))
-									.addChild(new Declaration("background-color").addChild(new VariableReference("LinkColor")))
-									.addChild((new ElseClause())
-											.addChild(new Declaration("background-color").addChild(new ColorLiteral("#000000")))
-
-									)
+		*/
+		stylesheet.addChild((new Stylerule())
+			.addChild(new TagSelector("p"))
+				.addChild((new Declaration("background-color"))
+					.addChild(new ColorLiteral("#ffffff")))
+				.addChild((new Declaration("width"))
+					.addChild(new VariableReference("ParWidth")))
+				.addChild((new IfClause())
+					.addChild(new VariableReference("AdjustColor"))
+					.addChild((new Declaration("color")
+							.addChild(new ColorLiteral("#124532"))))
+						.addChild((new IfClause())
+							.addChild(new VariableReference("UseLinkColor"))
+							.addChild(new Declaration("background-color").addChild(new VariableReference("LinkColor")))
+							.addChild((new ElseClause())
+								.addChild(new Declaration("background-color").addChild(new ColorLiteral("#000000")))
+							)
 					))
 					.addChild((new Declaration("height"))
-							.addChild(new PixelLiteral("20px")))
-        );
-        /*
-        a {
-	        color: LinkColor;
-        }
-        */
-        stylesheet.addChild((new Stylerule())
+						.addChild(new PixelLiteral("20px")))
+		);
+		/*
+		a {
+			color: LinkColor;
+		}
+		*/
+		stylesheet.addChild((new Stylerule())
 			.addChild(new TagSelector("a"))
 			.addChild((new Declaration("color"))
 				.addChild(new VariableReference("LinkColor"))
-            )
+			)
 		);
-        /*
-            #menu {
-        	width: ParWidth + 20px;
-            }
-        */
-        stylesheet.addChild((new Stylerule())
+		/*
+			#menu {
+			width: ParWidth + 20px;
+			}
+		*/
+		stylesheet.addChild((new Stylerule())
 			.addChild(new IdSelector("#menu"))
 			.addChild((new Declaration("width"))
 				.addChild((new AddOperation())
-                        .addChild(new VariableReference("ParWidth"))
-                        .addChild(new PixelLiteral("20px"))
-                )
-            )
-        );
-        /*
-
-
-         .menu {
+					.addChild(new VariableReference("ParWidth"))
+					.addChild(new PixelLiteral("20px"))
+				)
+			)
+		);
+		/*
+		 .menu {
 				color: #000000;
-    			background-color: LinkColor;
-
+				background-color: LinkColor;
 			}
-
-        */
-        stylesheet.addChild((new Stylerule())
+		*/
+		stylesheet.addChild((new Stylerule())
 			.addChild(new ClassSelector(".menu"))
 
 			.addChild((new Declaration("color"))
 				.addChild(new ColorLiteral("#000000"))
-            )
+			)
 				.addChild((new Declaration("background-color"))
-						.addChild(new VariableReference("LinkColor"))
+					.addChild(new VariableReference("LinkColor"))
 				)
-
 		);
 
 		return new AST(stylesheet);
--- src/test/java/nl/han/ica/icss/parser/ParserTest.java
+++ src/test/java/nl/han/ica/icss/parser/ParserTest.java
@@ -11,29 +11,33 @@ import org.antlr.v4.runtime.tree.*;
 
 import java.io.*;
 
-class ParserTest {
-
-	AST parseTestFile(String resource) throws IOException {
-
+class ParserTest
+{
+	AST parseTestFile(String resource) throws IOException
+	{
 		//Open test file to parse
 		ClassLoader classLoader = this.getClass().getClassLoader();
 
-    	InputStream inputStream = classLoader.getResourceAsStream(resource);
-        CharStream charStream = CharStreams.fromStream(inputStream);
-        ICSSLexer lexer = new ICSSLexer(charStream);
+		InputStream inputStream = classLoader.getResourceAsStream(resource);
+		CharStream charStream = CharStreams.fromStream(inputStream);
+		ICSSLexer lexer = new ICSSLexer(charStream);
 
-	    CommonTokenStream tokens = new CommonTokenStream(lexer);
+		CommonTokenStream tokens = new CommonTokenStream(lexer);
 
-        ICSSParser parser = new ICSSParser(tokens);
+		ICSSParser parser = new ICSSParser(tokens);
 		parser.setErrorHandler(new BailErrorStrategy());
 
 		//Setup collection of the parse error messages
 		BaseErrorListener errorListener = new BaseErrorListener() {
 			private String message;
-			public void syntaxError(Recognizer<?,?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
+
+			public void syntaxError(Recognizer<?,?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e)
+			{
 				message = msg;
 			}
-			public String toString() {
+
+			public String toString()
+			{
 				return message;
 			}
 		};
@@ -41,12 +45,15 @@ class ParserTest {
 		parser.addErrorListener(errorListener);
 
 		//Parse & extract AST
- 		ASTListener listener = new ASTListener();
-		try {
+		ASTListener listener = new ASTListener();
+		try
+		{
 			ParseTree parseTree = parser.stylesheet();
-            ParseTreeWalker walker = new ParseTreeWalker();
-            walker.walk(listener, parseTree);
-		} catch(ParseCancellationException e) {
+			ParseTreeWalker walker = new ParseTreeWalker();
+			walker.walk(listener, parseTree);
+		}
+		catch(ParseCancellationException e)
+		{
 			fail(errorListener.toString());
 		}
 
@@ -54,31 +61,37 @@ class ParserTest {
 	}
 
 	@Test
-	void testParseLevel0() throws IOException {
-
+	void testParseLevel0() throws IOException
+	{
 		AST sut = parseTestFile("level0.icss");
 		AST exp = Fixtures.uncheckedLevel0();
+
 		assertEquals(exp,sut);
 	}
-	@Test
-	void testParseLevel1() throws IOException {
 
+	@Test
+	void testParseLevel1() throws IOException
+	{
 		AST sut = parseTestFile("level1.icss");
 		AST exp = Fixtures.uncheckedLevel1();
+
 		assertEquals(exp,sut);
 	}
-	@Test
-	void testParseLevel2() throws IOException {
 
+	@Test
+	void testParseLevel2() throws IOException
+	{
 		AST sut = parseTestFile("level2.icss");
 		AST exp = Fixtures.uncheckedLevel2();
+
 		assertEquals(exp,sut);
 	}
 	@Test
-	void testParseLevel3() throws IOException {
-
+	void testParseLevel3() throws IOException
+	{
 		AST sut = parseTestFile("level3.icss");
 		AST exp = Fixtures.uncheckedLevel3();
+
 		assertEquals(exp,sut);
 	}
 }
