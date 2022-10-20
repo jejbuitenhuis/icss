4e2c66149577f030e8ea6d7449c362bbf7b5c2c7
jejbuitenhuis <jorambuitenhuis@gmail.com>
2022-10-20 20:09:57 +0200

    Fix OperationChecker allowing Scalars everywhere

--- src/main/java/nl/han/ica/icss/checker/checkers/OperationChecker.java
+++ src/main/java/nl/han/ica/icss/checker/checkers/OperationChecker.java
@@ -8,6 +8,7 @@ import nl.han.ica.datastructures.IHANLinkedList;
 import nl.han.ica.icss.ast.ASTNode;
 import nl.han.ica.icss.ast.Expression;
 import nl.han.ica.icss.ast.Operation;
+import nl.han.ica.icss.ast.operations.MultiplyOperation;
 import nl.han.ica.icss.ast.types.ExpressionType;
 
 public class OperationChecker implements CheckerFunction
@@ -30,9 +31,8 @@ public class OperationChecker implements CheckerFunction
 		Expression left = node.lhs;
 		Expression right = node.rhs;
 
-		ExpressionType typeLeft = ExpressionType.fromExpression(left, variableTypes);
-
 		// Check if left and right expressions aren't invalid types {{{
+		ExpressionType typeLeft = ExpressionType.fromExpression(left, variableTypes);
 
 		// if left or right are an Operation, they will get checked later,
 		// because we walk recursively through the AST
@@ -73,20 +73,38 @@ public class OperationChecker implements CheckerFunction
 
 		// Check if left and right expressions are the same ExpressionTypes {{{
 
-		// If one of the expressions is a scalar, it doesn't matter what the
-		// other type is, because `scalar * something` is always allowed
-		if (typeLeft == ExpressionType.SCALAR) return;
-		if (typeRight == ExpressionType.SCALAR) return;
+		// only the MultiplyOperation allows for Scalars
+		if (node instanceof MultiplyOperation)
+		{
+			// one of the sides is a Scalar, so it doesn't matter what the
+			// other side is
+			if (
+				typeLeft == ExpressionType.SCALAR
+				|| typeRight == ExpressionType.SCALAR
+			)
+				return;
 
+			// only non-scalars are left, which means the MultiplyOperation is
+			// never allowed (e.g. % * %, px * px, % * px, etc)
+			node.setError( String.format(
+				"Cannot use type '%s' in combination with type '%s' in '%s'",
+				typeLeft,
+				typeRight,
+				node.getClass().getSimpleName()
+			) );
+
+			return; // only one error per node allowed
+		}
+
+		// left and right need to be of the same type
 		if (typeLeft != typeRight)
 		{
-			String error = String.format(
-				"Types in operation must be the same, but got '%s' and '%s'",
+			node.setError( String.format(
+				"Cannot use type '%s' in combination with type '%s' in '%s'",
 				typeLeft,
-				typeRight
-			);
-
-			node.setError(error);
+				typeRight,
+				node.getClass().getSimpleName()
+			) );
 
 			return; // only one error per node allowed
 		}
